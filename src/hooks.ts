4040567c7f83bb9250f4809bf47e23ae44cd3a9e
Phillip Malboeuf <phil@phils.computer>
2022-04-16 20:55:15 -0400

    link fonts fix

--- src/hooks.ts
+++ src/hooks.ts
@@ -5,7 +5,7 @@ import type { Handle } from '@sveltejs/kit';
 export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event)
 
-	response.headers.set('Link', '<%svelte.assets%/ReallySansSmallStudent-Semibold.woff2>; rel="preload"; as="font"; type="font/woff2"; crossorigin="anonymous",<%svelte.assets%/MaisonNeueExtendedWEB-Bold.woff2>; rel="preload"; as="font"; type="font/woff2"; crossorigin="anonymous",<%svelte.assets%/MaisonNeueExtendedWEB-Demi.woff2>; rel="preload"; as="font"; type="font/woff2"; crossorigin="anonymous"')
+	response.headers.set('Link', '<./ReallySansSmallStudent-Semibold.woff2>; rel="preload"; as="font"; type="font/woff2"; crossorigin="anonymous",<./MaisonNeueExtendedWEB-Bold.woff2>; rel="preload"; as="font"; type="font/woff2"; crossorigin="anonymous",<./MaisonNeueExtendedWEB-Demi.woff2>; rel="preload"; as="font"; type="font/woff2"; crossorigin="anonymous"')
 
 	return response
 }
