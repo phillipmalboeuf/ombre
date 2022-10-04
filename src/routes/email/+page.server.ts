ae654271937c70e343d40661a26b032ad6a483bd
Phillip Malboeuf <phil@phils.computer>
2022-10-04 16:25:32 -0400

    email form page

---
+++ src/routes/email/+page.server.ts
@@ -0,0 +1,62 @@
+import { error, redirect, type Actions } from '@sveltejs/kit'
+
+import { query } from '$lib/clients/payload'
+import type { Customer, Product } from '$lib/payload-types'
+import type { PageServerLoad } from '.svelte-kit/types/src/routes/account/$types'
+import { TOKEN_KEY } from '$env/static/private'
+import { formData } from '$lib/form'
+import { randomPassword } from '$lib/encryption'
+
+export const load: PageServerLoad = async ({ url, locals, request }) => {
+  // const me = (await query<{ meCustomer: { user : Customer }}>(fetch, `
+  //     query {
+  //       meCustomer {
+  //         user {
+  //           email
+  //           name
+  //         }
+  //         exp
+  //       }
+  //     }
+  //   `, {}, locals.token)).data.meCustomer
+
+  // if (!locals.token) {
+  //   throw redirect(307, '/account/login')
+  // }
+
+  // const me = JSON.parse(Buffer.from(locals.token.split('.')[1], 'base64').toString())
+  // console.log(me)
+
+  return {
+    // me: locals.customer
+  }
+}
+
+export const actions: Actions = {
+  default: async ({ request, cookies }) => {
+    const { name, email } = await formData(request)
+
+    const today = new Date()
+    const id = `${today.getFullYear()}_${today.getMonth()}_${randomPassword(3)}`
+    const password = randomPassword(6)
+
+    const customer = await query<{ createCustomer: { email: string, password: string }}>(fetch, `
+        mutation($id: String!, $name: String, $email: String!, $password: String!) {
+          createCustomer(data: { id: $id, email: $email, password: $password, name: $name }) {
+            id
+            name
+            email
+          }
+        }
+      `, { id, name, email, password })
+      
+    if (customer.errors) {
+      throw error(403, customer.errors[0].message)
+    }
+
+    
+    // return {
+    //   location: '/account'
+    // }
+  },
+}
---
+++ src/routes/email/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { enhance } from '$app/forms'
+  import type { PageServerData } from './$types'
+
+  // export let data: PageServerData
+	let success = false
+</script>
+
+{#if success}
+<h2>Merci!</h2>
+<button class="button--full" on:click={() => success = false}>Inscrire un nouveau client</button>
+{:else}
+<form method="POST" use:enhance={() => {
+  return async ({ result, update }) => {
+    if (result.type === 'success') {
+      success = true
+    }
+  }
+}}>
+  <label for="name">Nom</label>
+  <input type="text" name="name" id="name">
+
+  <label for="email">Adresse courriel</label>
+  <input type="email" name="email" id="email">
+
+  <button class="button--full button--dark" type="submit">S'inscrire</button>
+</form>
+{/if}
+
+<style lang="scss">
+	// section {
+	// 	width: 100%;
+	// 	max-width: var(--step-7);
+	// 	margin: 0 auto;
+	// }
+</style>
