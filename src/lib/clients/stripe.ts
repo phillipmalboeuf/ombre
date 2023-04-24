98ffd41a2411cfe2a1a3481051ef77ed051763cf
Phillip Malboeuf <phil@phils.computer>
2023-04-24 11:31:26 -0400

    quote?

--- src/lib/clients/stripe.ts
+++ src/lib/clients/stripe.ts
@@ -3,5 +3,6 @@ import Stripe from 'stripe'
 
 export const stripe = new Stripe(STRIPE_SK, { 
   // betas: ['process_order_beta_1'],
-  apiVersion: "2022-08-01;orders_beta=v4" as "2022-08-01"
+  apiVersion: "2022-08-01;orders_beta=v4" as "2022-08-01",
+  maxNetworkRetries: 3,
  })
--- src/routes/checkout/intent/+server.ts
+++ src/routes/checkout/intent/+server.ts
@@ -20,122 +20,170 @@ export const POST: RequestHandler = async ({ request, url }) => {
   const today = new Date()
   const id = `${today.getFullYear()}_${today.getMonth()}_${randomPassword(3)}`
 
-  if (interval === 'one-time') {
-    let invoice = await stripe.invoices.create({
-      customer,
-      auto_advance: false,
-      discounts: coupon ? [{ coupon }] : [],
-      metadata: {
-        id,
-        kiosk: kiosk.id,
-        deliver_at: DateTime.now().set({ weekday: 4 }).plus({ days: kiosk.minimum_order_days }).toISO()
-      },
-      expand: ['payment_intent'],
-      currency: 'CAD',
-      collection_method: 'charge_automatically',
-      payment_settings: {
-        payment_method_types: ['card', 'acss_debit'],
-        payment_method_options: {
-          acss_debit: {
-            mandate_options: {
-              // payment_schedule: 'combined',
-              // interval_description: 'when any invoice becomes due',
-              transaction_type: 'personal',
-            },
-          },
-        },
-      },
-    }, {
-      stripeAccount: 'acct_1LpynD4IazTmFSzT'
-    })
-
-    for (let index = 0; index < items.length; index++) {
-      const { product, bundle, price, size, quantity } = items[index]
-
-      await stripe.invoiceItems.create({
-        invoice: invoice.id,
-        customer,
-        price_data: {
-          product: {'Radis Mauve': 'radis-mauve', 'Découverte': 'decouverte'}[product || bundle] || (product || bundle),
-          currency: 'CAD',
-          unit_amount: price * 100
-        },
-        quantity,
-        metadata: {
-          size,
-        },
-      }, {
-        stripeAccount: 'acct_1LpynD4IazTmFSzT'
-      })
-    }
-
-    invoice = await stripe.invoices.finalizeInvoice(invoice.id, {
-      auto_advance: false,
-      expand: ['payment_intent'],
-    }, {
-      stripeAccount: 'acct_1LpynD4IazTmFSzT'
-    })
-    console.log(invoice)
-  
-    if (invoice) {
-      return json(invoice)
-    }
-  }
-
   const split = interval.split('-')
-  const subscription = await stripe.subscriptions.create({
+  const quote = await stripe.quotes.create({
     customer,
-    coupon,
-    items: items
-      .map(({ size, quantity, product, bundle, price }) => 
-      ({
-        price_data: {
-          product: {'Radis Mauve': 'radis-mauve', 'Découverte': 'decouverte'}[product || bundle] || (product || bundle),
-          unit_amount: price * 100,
-          currency: 'CAD',
-          recurring: {
-            interval: split[0] as 'week',
-            interval_count: parseInt(split[1])
-          },
-        },
-        metadata: {
-          size,
-        },
-        quantity
-      })),
+    discounts: coupon ? [{ coupon }] : [],
     metadata: {
       id,
       kiosk: kiosk.id,
       deliver_at: DateTime.now().set({ weekday: 4 }).plus({ days: kiosk.minimum_order_days }).toISO()
     },
-    // trial_end: DateTime.now().set({ weekday: 4 }).plus({ days: kiosk.minimum_order_days }).toUnixInteger(),
-    expand: ['latest_invoice.payment_intent'],
-    currency: 'CAD',
-    payment_behavior: 'default_incomplete',
-    // transfer_data: {
-    //   destination: 'acct_1LpynD4IazTmFSzT'
+    // currency: 'CAD',
+    collection_method: 'charge_automatically',
+    // payment_settings: {
+    //   payment_method_types: ['card', 'acss_debit'],
+    //   payment_method_options: {
+    //     acss_debit: {
+    //       mandate_options: {
+    //         // payment_schedule: 'combined',
+    //         // interval_description: 'when any invoice becomes due',
+    //         transaction_type: 'personal',
+    //       },
+    //     },
+    //   },
     // },
-    payment_settings: {
-      save_default_payment_method: 'on_subscription',
-      payment_method_types: ['card', 'acss_debit'],
-      payment_method_options: {
-        acss_debit: {
-          mandate_options: {
-            // payment_schedule: '',
-            // interval_description: 'when any invoice becomes due',
-            transaction_type: 'personal',
-          },
-        },
+    line_items: items.map(({ product, bundle, price, size, quantity }) => ({
+      price_data: {
+        product: {'Radis Mauve': 'radis-mauve', 'Découverte': 'decouverte'}[product || bundle] || (product || bundle),
+        currency: 'CAD',
+        unit_amount: price * 100,
+        ...interval !== 'one-time' && {
+          recurring: {
+            interval: split[0] as 'week',
+            interval_count: parseInt(split[1])
+          }
+        }
       },
-    },
+      quantity,
+      metadata: {
+        size,
+      },
+    }))
   }, {
     stripeAccount: 'acct_1LpynD4IazTmFSzT'
   })
 
-  console.log(subscription)
+  const { invoice } = await stripe.quotes.accept(quote.id, { expand: ['invoice.payment_intent'] }, {
+    stripeAccount: 'acct_1LpynD4IazTmFSzT'
+  })
+  console.log(invoice)
 
-  if (subscription) {
-    return json(subscription)
-  }
+  return json(invoice)
+
+  // if (interval === 'one-time') {
+  //   let invoice = await stripe.invoices.create({
+  //     customer,
+  //     auto_advance: false,
+  //     discounts: coupon ? [{ coupon }] : [],
+  //     metadata: {
+  //       id,
+  //       kiosk: kiosk.id,
+  //       deliver_at: DateTime.now().set({ weekday: 4 }).plus({ days: kiosk.minimum_order_days }).toISO()
+  //     },
+  //     expand: ['payment_intent'],
+  //     currency: 'CAD',
+  //     collection_method: 'charge_automatically',
+  //     payment_settings: {
+  //       payment_method_types: ['card', 'acss_debit'],
+  //       payment_method_options: {
+  //         acss_debit: {
+  //           mandate_options: {
+  //             // payment_schedule: 'combined',
+  //             // interval_description: 'when any invoice becomes due',
+  //             transaction_type: 'personal',
+  //           },
+  //         },
+  //       },
+  //     },
+  //   }, {
+  //     stripeAccount: 'acct_1LpynD4IazTmFSzT'
+  //   })
+
+  //   await Promise.all(items.map(({ product, bundle, price, size, quantity }) => stripe.invoiceItems.create({
+  //       invoice: invoice.id,
+  //       customer,
+  //       price_data: {
+  //         product: {'Radis Mauve': 'radis-mauve', 'Découverte': 'decouverte'}[product || bundle] || (product || bundle),
+  //         currency: 'CAD',
+  //         unit_amount: price * 100
+  //       },
+  //       quantity,
+  //       metadata: {
+  //         size,
+  //       },
+  //     }, {
+  //       stripeAccount: 'acct_1LpynD4IazTmFSzT'
+  //     })
+  //   ))
+
+  //   invoice = await stripe.invoices.finalizeInvoice(invoice.id, {
+  //     auto_advance: false,
+  //     expand: ['payment_intent'],
+  //   }, {
+  //     stripeAccount: 'acct_1LpynD4IazTmFSzT'
+  //   })
+  //   console.log(invoice)
+  
+  //   if (invoice) {
+  //     return json(invoice)
+  //   }
+  // }
+
+  // const split = interval.split('-')
+  // const subscription = await stripe.subscriptions.create({
+  //   customer,
+  //   coupon,
+  //   items: items
+  //     .map(({ size, quantity, product, bundle, price }) => 
+  //     ({
+  //       price_data: {
+  //         product: {'Radis Mauve': 'radis-mauve', 'Découverte': 'decouverte'}[product || bundle] || (product || bundle),
+  //         unit_amount: price * 100,
+  //         currency: 'CAD',
+  //         recurring: {
+  //           interval: split[0] as 'week',
+  //           interval_count: parseInt(split[1])
+  //         },
+  //       },
+  //       metadata: {
+  //         size,
+  //       },
+  //       quantity
+  //     })),
+  //   metadata: {
+  //     id,
+  //     kiosk: kiosk.id,
+  //     deliver_at: DateTime.now().set({ weekday: 4 }).plus({ days: kiosk.minimum_order_days }).toISO()
+  //   },
+  //   // trial_end: DateTime.now().set({ weekday: 4 }).plus({ days: kiosk.minimum_order_days }).toUnixInteger(),
+  //   expand: ['latest_invoice.payment_intent'],
+  //   currency: 'CAD',
+  //   payment_behavior: 'default_incomplete',
+  //   // transfer_data: {
+  //   //   destination: 'acct_1LpynD4IazTmFSzT'
+  //   // },
+  //   payment_settings: {
+  //     save_default_payment_method: 'on_subscription',
+  //     payment_method_types: ['card', 'acss_debit'],
+  //     payment_method_options: {
+  //       acss_debit: {
+  //         mandate_options: {
+  //           // payment_schedule: '',
+  //           // interval_description: 'when any invoice becomes due',
+  //           transaction_type: 'personal',
+  //         },
+  //       },
+  //     },
+  //   },
+  // }, {
+  //   stripeAccount: 'acct_1LpynD4IazTmFSzT'
+  // })
+
+  // console.log(subscription)
+
+  // if (subscription) {
+  //   return json(subscription)
+  // }
 }
 
